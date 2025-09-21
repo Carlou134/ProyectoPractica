ecc4a1cb9039b0d0c456ce40e6cf585a3bc2a45d
Carlou134 <carlouvasquez134@gmail.com>
2025-09-21 13:52:54 -0500

    Componentes principales

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  user = {
+    name: "Carlos",
+    pictureProfile: "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=",
+    hobbies: [
+      "Programar",
+      "Bailar",
+      "Jugar Fuchibol"
+    ]
+  };
 
+  title = 'ProyectoPractica';
+
+  numero = 1;
 }
